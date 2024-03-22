ae5db28e662ac3bcbc1be937168c79d17d773c42
beyond <beyondlov1@hotmail.com>
2024-03-22 22:33:04 +0800

    add

--- plugins/user.lua
+++ plugins/user.lua
@@ -57,5 +57,24 @@ return {
         --   desc = "Treesitter Search",
         -- },
       },
+    },
+    {
+      "lukas-reineke/indent-blankline.nvim",
+      main = "ibl", 
+      event = "User AstroFile",
+      opts = {
+        indent = {
+          char = "│",
+        },
+        scope = {
+          enabled = false,
+        },
+        exclude = {
+          filetypes = { "help", "alpha", "dashboard", "Trouble", "lazy", "neo-tree" },
+        },
+        whitespace = {
+          remove_blankline_trail = true,
+        },
+      },
     }
   }
