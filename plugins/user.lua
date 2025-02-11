2134b05d688c206632c817f89d4d6c99b54f48c6
beyond <beyondlov1@hotmail.com>
2025-02-11 14:34:17 +0800

    add

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,22 +1,32 @@
 return {
-    {
+  {
     "git@gitee.com:beyondlov1/bookmarks.nvim.git",
     dependencies = {
-      {"stevearc/dressing.nvim"} -- optional: to have the same UI shown in the GIF
+      { "stevearc/dressing.nvim" }, -- optional: to have the same UI shown in the GIF
     },
     event = "VimEnter",
-    config = function ()
+    config = function()
       require("bookmarks").setup {
-        json_db_path = vim.fs.normalize(vim.fn.stdpath("config") .. "/bookmarks.db.json"),
+        json_db_path = vim.fs.normalize(vim.fn.stdpath "config" .. "/bookmarks.db.json"),
         signs = {
           mark = { icon = "⚑", color = "grey" },
         },
       }
-      vim.keymap.set({ "n", "v" }, "bb", "<cmd>BookmarksMarkSimple<cr>", { desc = "Mark current line into active BookmarkList." })
-      vim.keymap.set({ "n", "v" }, "bl", "<cmd>BookmarksGoto<cr>", { desc = "Go to bookmark at current active BookmarkList" })
+      vim.keymap.set(
+        { "n", "v" },
+        "bb",
+        "<cmd>BookmarksMarkSimple<cr>",
+        { desc = "Mark current line into active BookmarkList." }
+      )
+      vim.keymap.set(
+        { "n", "v" },
+        "bl",
+        "<cmd>BookmarksGoto<cr>",
+        { desc = "Go to bookmark at current active BookmarkList" }
+      )
       -- vim.keymap.set({ "n", "v" }, "ma", "<cmd>BookmarksCommands<cr>", { desc = "Find and trigger a bookmark command." })
       -- vim.keymap.set({ "n", "v" }, "mg", "<cmd>BookmarksGotoRecent<cr>", { desc = "Go to latest visited/created Bookmark" })
-    end
+    end,
   },
   -- {
   --   "tomasky/bookmarks.nvim",
@@ -38,7 +48,7 @@ return {
   --         local bm = require "bookmarks"
   --         local tel = require "telescope"
   --         local map = vim.keymap.set
-          -- map("n","bm",bm.bookmark_toggle) -- add or remove bookmark at current line
+  -- map("n","bm",bm.bookmark_toggle) -- add or remove bookmark at current line
   --         map("n","bi",bm.bookmark_ann) -- add or edit mark annotation at current line
   --         map("n","bc",bm.bookmark_clean) -- clean all marks in local buffer
   --         map("n","bn",bm.bookmark_next) -- jump to next mark in local buffer
@@ -60,75 +70,97 @@ return {
   --   end,
   -- },
   -- 'preservim/nerdcommenter',
-    -- {
-    --   "ggandor/leap.nvim",
-    --   keys = {
-    --     { "f", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "Leap forwari to" },
-    --     { "<s-f>", "<Plug>(leap-backward-to)", mode = { "n", "x", "o" }, desc = "Leap backward to" },
-    --     -- { "x", "<Plug>(leap-forward-till)", mode = { "x", "o" }, desc = "Leap forward till" },
-    --     -- { "X", "<Plug>(leap-backward-till)", mode = { "x", "o" }, desc = "Leap backward till" },
-    --     -- { "S", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from window" },
-    --   },
-    -- },
-    {
-      "folke/flash.nvim",
-      event = "VeryLazy",
-      vscode = true,
-      opts = {
-        modes = {
-          char = {
-            keys = {  "t", "T", "," },
-          }
-        },
-      },
-      keys = {
-        {
-          "f",
-          mode = { "n", "x", "o" },
-          function() require("flash").jump() end,
-          desc = "Flash",
-        },
-        {
-          "F",
-          mode = { "n", "o", "x" },
-          function() require("flash").treesitter() end,
-          desc = "Flash Treesitter",
-        },
-        -- {
-        --   "r",
-        --   mode = "o",
-        --   function() require("flash").remote() end,
-        --   desc = "Remote Flash",
-        -- },
-        -- {
-        --   "R",
-        --   mode = { "o", "x" },
-        --   function() require("flash").treesitter_search() end,
-        --   desc = "Treesitter Search",
-        -- },
-      },
-    },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl", 
-      event = "User AstroFile",
-      opts = {
-        indent = {
-          char = "│",
-        },
-        scope = {
-          enabled = false,
-        },
-        exclude = {
-          filetypes = { "help", "alpha", "dashboard", "Trouble", "lazy", "neo-tree" },
-        },
-        whitespace = {
-          remove_blankline_trail = true,
+  -- {
+  --   "ggandor/leap.nvim",
+  --   keys = {
+  --     { "f", "<Plug>(leap-forward-to)", mode = { "n", "x", "o" }, desc = "Leap forwari to" },
+  --     { "<s-f>", "<Plug>(leap-backward-to)", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+  --     -- { "x", "<Plug>(leap-forward-till)", mode = { "x", "o" }, desc = "Leap forward till" },
+  --     -- { "X", "<Plug>(leap-backward-till)", mode = { "x", "o" }, desc = "Leap backward till" },
+  --     -- { "S", "<Plug>(leap-from-window)", mode = { "n", "x", "o" }, desc = "Leap from window" },
+  --   },
+  -- },
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    vscode = true,
+    opts = {
+      modes = {
+        char = {
+          keys = { "t", "T", "," },
         },
       },
     },
-    {
-      "git@gitee.com:beyondlov1/fcitx.nvim.git",
-      event = "User AstroFile",
-    }
-  }
+    keys = {
+      {
+        "f",
+        mode = { "n", "x", "o" },
+        function() require("flash").jump() end,
+        desc = "Flash",
+      },
+      {
+        "F",
+        mode = { "n", "o", "x" },
+        function() require("flash").treesitter() end,
+        desc = "Flash Treesitter",
+      },
+      -- {
+      --   "r",
+      --   mode = "o",
+      --   function() require("flash").remote() end,
+      --   desc = "Remote Flash",
+      -- },
+      -- {
+      --   "R",
+      --   mode = { "o", "x" },
+      --   function() require("flash").treesitter_search() end,
+      --   desc = "Treesitter Search",
+      -- },
+    },
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = "User AstroFile",
+    opts = {
+      indent = {
+        char = "│",
+      },
+      scope = {
+        enabled = false,
+      },
+      exclude = {
+        filetypes = { "help", "alpha", "dashboard", "Trouble", "lazy", "neo-tree" },
+      },
+      whitespace = {
+        remove_blankline_trail = true,
+      },
+    },
+  },
+  {
+    "git@gitee.com:beyondlov1/fcitx.nvim.git",
+    event = "User AstroFile",
+  },
+  {
+    "stevearc/conform.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    event = { "BufWritePre" },
+    -- Customize or remove this keymap to your liking
+    keys = {
+      {
+        "<leader>f",
+        function() require("conform").format { async = true } end,
+        mode = "",
+        desc = "格式化代码",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "isort", "black" },
+      },
+    },
+  },
+}
